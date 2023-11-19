797f696f3066dcbcd5950c84d6cf1d7019293d51
Le Bronn <lebronn.samson@cit.edu>
2023-11-19 19:25:54 +0800

    Add project files.

---
+++ ImageProcessing-Part1/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ImageProcessing_Part1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            menuStrip1 = new MenuStrip();
+            addFileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            closeToolStripMenuItem = new ToolStripMenuItem();
+            imageProcessToolStripMenuItem = new ToolStripMenuItem();
+            basicCopyToolStripMenuItem = new ToolStripMenuItem();
+            greyscaleToolStripMenuItem = new ToolStripMenuItem();
+            colorInversionToolStripMenuItem = new ToolStripMenuItem();
+            histogramToolStripMenuItem = new ToolStripMenuItem();
+            scepiaToolStripMenuItem = new ToolStripMenuItem();
+            openFileDialog1 = new OpenFileDialog();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            saveFileDialog1 = new SaveFileDialog();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(menuStrip1, "menuStrip1");
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { addFileToolStripMenuItem, imageProcessToolStripMenuItem });
+            menuStrip1.Name = "menuStrip1";
+            // 
+            // addFileToolStripMenuItem
+            // 
+            resources.ApplyResources(addFileToolStripMenuItem, "addFileToolStripMenuItem");
+            addFileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, saveToolStripMenuItem, closeToolStripMenuItem });
+            addFileToolStripMenuItem.Name = "addFileToolStripMenuItem";
+            // 
+            // openToolStripMenuItem
+            // 
+            resources.ApplyResources(openToolStripMenuItem, "openToolStripMenuItem");
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            resources.ApplyResources(saveToolStripMenuItem, "saveToolStripMenuItem");
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            // 
+            // closeToolStripMenuItem
+            // 
+            resources.ApplyResources(closeToolStripMenuItem, "closeToolStripMenuItem");
+            closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            closeToolStripMenuItem.Click += closeToolStripMenuItem_Click;
+            // 
+            // imageProcessToolStripMenuItem
+            // 
+            resources.ApplyResources(imageProcessToolStripMenuItem, "imageProcessToolStripMenuItem");
+            imageProcessToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { basicCopyToolStripMenuItem, greyscaleToolStripMenuItem, colorInversionToolStripMenuItem, histogramToolStripMenuItem, scepiaToolStripMenuItem });
+            imageProcessToolStripMenuItem.Name = "imageProcessToolStripMenuItem";
+            // 
+            // basicCopyToolStripMenuItem
+            // 
+            resources.ApplyResources(basicCopyToolStripMenuItem, "basicCopyToolStripMenuItem");
+            basicCopyToolStripMenuItem.Name = "basicCopyToolStripMenuItem";
+            basicCopyToolStripMenuItem.Click += basicCopyToolStripMenuItem_Click;
+            // 
+            // greyscaleToolStripMenuItem
+            // 
+            resources.ApplyResources(greyscaleToolStripMenuItem, "greyscaleToolStripMenuItem");
+            greyscaleToolStripMenuItem.Name = "greyscaleToolStripMenuItem";
+            greyscaleToolStripMenuItem.Click += greyscaleToolStripMenuItem_Click;
+            // 
+            // colorInversionToolStripMenuItem
+            // 
+            resources.ApplyResources(colorInversionToolStripMenuItem, "colorInversionToolStripMenuItem");
+            colorInversionToolStripMenuItem.Name = "colorInversionToolStripMenuItem";
+            colorInversionToolStripMenuItem.Click += colorInversionToolStripMenuItem_Click;
+            // 
+            // histogramToolStripMenuItem
+            // 
+            resources.ApplyResources(histogramToolStripMenuItem, "histogramToolStripMenuItem");
+            histogramToolStripMenuItem.Name = "histogramToolStripMenuItem";
+            histogramToolStripMenuItem.Click += histogramToolStripMenuItem_Click;
+            // 
+            // scepiaToolStripMenuItem
+            // 
+            resources.ApplyResources(scepiaToolStripMenuItem, "scepiaToolStripMenuItem");
+            scepiaToolStripMenuItem.Name = "scepiaToolStripMenuItem";
+            scepiaToolStripMenuItem.Click += scepiaToolStripMenuItem_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            resources.ApplyResources(openFileDialog1, "openFileDialog1");
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(pictureBox1, "pictureBox1");
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(pictureBox2, "pictureBox2");
+            pictureBox2.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.TabStop = false;
+            // 
+            // saveFileDialog1
+            // 
+            resources.ApplyResources(saveFileDialog1, "saveFileDialog1");
+            saveFileDialog1.FileOk += saveFileDialog1_FileOk;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem addFileToolStripMenuItem;
+        private ToolStripMenuItem imageProcessToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem basicCopyToolStripMenuItem;
+        private ToolStripMenuItem greyscaleToolStripMenuItem;
+        private ToolStripMenuItem colorInversionToolStripMenuItem;
+        private ToolStripMenuItem histogramToolStripMenuItem;
+        private ToolStripMenuItem scepiaToolStripMenuItem;
+        private OpenFileDialog openFileDialog1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private ToolStripMenuItem closeToolStripMenuItem;
+        private SaveFileDialog saveFileDialog1;
+    }
+}
---
+++ ImageProcessing-Part1/Form1.cs
@@ -0,0 +1,182 @@
+namespace ImageProcessing_Part1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image, newImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            image = new Bitmap(openFileDialog1.FileName);
+
+            pictureBox1.Image = image;
+        }
+
+        private void basicCopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newImage = new Bitmap(image.Width, image.Height);
+
+            for (int i = 0; i < newImage.Width; i++)
+            {
+                for (int j = 0; j < newImage.Height; j++)
+                {
+                    Color imageColor = image.GetPixel(i, j);
+                    newImage.SetPixel(i, j, imageColor);
+                }
+            }
+
+            pictureBox2.Image = newImage;
+            image = newImage;
+        }
+
+        private void greyscaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newImage = new Bitmap(image.Width, image.Height);
+
+            for (int i = 0; i < newImage.Width; i++)
+            {
+                for (int j = 0; j < newImage.Height; j++)
+                {
+                    Color changeColor = image.GetPixel(i, j);
+                    int gray = (int)(changeColor.R + changeColor.G + changeColor.B) / 3;
+                    newImage.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
+                }
+            }
+
+            pictureBox2.Image = newImage;
+            image = newImage;
+        }
+
+        private void colorInversionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newImage = new Bitmap(image.Width, image.Height);
+
+            for (int i = 0; i < newImage.Width; i++)
+            {
+                for (int j = 0; j < newImage.Height; j++)
+                {
+                    Color changeColor = image.GetPixel(i, j);
+                    newImage.SetPixel(i, j, Color.FromArgb(255 - changeColor.R, 255 - changeColor.G, 255 - changeColor.B));
+                }
+            }
+
+            pictureBox2.Image = newImage;
+            image = newImage;
+        }
+
+        private void histogramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newImage = new Bitmap(image.Width, image.Height);
+
+            for (int i = 0; i < newImage.Width; i++)
+            {
+                for (int j = 0; j < newImage.Height; j++)
+                {
+                    Color changeColor = image.GetPixel(i, j);
+                    int gray = (int)(changeColor.R + changeColor.G + changeColor.B) / 3;
+                    newImage.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
+                }
+            }
+
+            int[] histogramData = new int[256];
+            for (int i = 0; i < newImage.Width; i++)
+            {
+                for (int j = 0; j < newImage.Height; j++)
+                {
+                    Color greyscaleData = newImage.GetPixel(i, j);
+                    histogramData[greyscaleData.R]++;
+                }
+            }
+
+            Bitmap histogramGraph = new Bitmap(256, 800);
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < 800; j++)
+                {
+                    histogramGraph.SetPixel(i, j, Color.White);
+                }
+            }
+
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < Math.Min(histogramData[i] / 5, 800); j++)
+                {
+                    histogramGraph.SetPixel(i, j, Color.White);
+                }
+            }
+
+            pictureBox2.Image = histogramGraph;
+        }
+
+        private void scepiaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newImage = new Bitmap(image.Width, image.Height);
+
+            for (int i = 0; i < newImage.Width; i++)
+            {
+                for (int j = 0; j < newImage.Height; j++)
+                {
+                    Color changeColor = image.GetPixel(i, j);
+                    int red = (int)(changeColor.R);
+                    int green = (int)(changeColor.G);
+                    int blue = (int)(changeColor.B);
+                    int sepiaRed = (int)((0.393 * red) + (0.769 * green) + (0.189 * blue));
+                    int sepiaGreen = (int)((0.349 * red) + (0.686 * green) + (0.168 * blue));
+                    int sepiaBlue = (int)((0.272 * red) + (0.534 * green) + (0.131 * blue));
+
+                    if (sepiaRed > 255)
+                    {
+                        red = 255;
+                    }
+                    else
+                    {
+                        red = sepiaRed;
+                    }
+
+                    if (sepiaGreen > 255)
+                    {
+                        green = 255;
+                    }
+                    else
+                    {
+                        green = sepiaGreen;
+                    }
+
+                    if (sepiaBlue > 255)
+                    {
+                        blue = 255;
+                    }
+                    else
+                    {
+                        blue = sepiaBlue;
+                    }
+
+                    newImage.SetPixel(i, j, Color.FromArgb(red, green, blue));
+                }
+            }
+
+            pictureBox2.Image = newImage;
+            image = newImage;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            newImage.Save(saveFileDialog1.FileName);
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
